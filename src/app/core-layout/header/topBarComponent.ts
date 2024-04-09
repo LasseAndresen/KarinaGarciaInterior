a680fd114e38d40315f7ba901761ff2459eed779
Lasse <lasse.andresen9@gmail.com>
2024-04-09 22:02:46 +0200

    Implemented mobile menu

--- src/app/core-layout/header/topBarComponent.ts
+++ src/app/core-layout/header/topBarComponent.ts
@@ -1,22 +1,40 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import {Component, Input, OnInit, OnDestroy, afterNextRender} from '@angular/core';
+import {fromEvent, Observable, Subscription, throttleTime} from 'rxjs';
 import {NavigationButtonComponent} from "../../components/navigation-button/navigationButtonComponent";
+import {NgClass, NgIf, NgTemplateOutlet} from "@angular/common";
 
 @Component({
   selector: 'page-header',
   templateUrl: './topBarComponent.html',
   styleUrls: ['./topBarComponent.scss'],
   imports: [
-    NavigationButtonComponent
+    NavigationButtonComponent,
+    NgIf,
+    NgTemplateOutlet,
+    NgClass
   ],
   standalone: true
 })
 export class TopBarComponent implements OnInit, OnDestroy {
   private _subscriptions: Subscription[] = [];
   protected logoSrc: string = 'assets/Placeholder.png';
+  protected showMobileMenu = false;
+  protected screenWidth = 800;
 
   constructor(
-  ) {}
+  ) {
+    // Checks if screen size is less than 1024 pixels
+    const checkScreenSize = () => {this.screenWidth = document.body.offsetWidth;
+      console.log('screenWidth: ' + this.screenWidth);}
+
+    afterNextRender(() => {
+      // Run initial check
+      checkScreenSize();
+
+      // Create observable from window resize event throttled so only fires every 500ms
+      this._subscriptions.push(fromEvent(window, 'resize').pipe(throttleTime(500)).subscribe(checkScreenSize));
+    });
+  }
 
   public ngOnInit() {
   }
@@ -26,4 +44,12 @@ export class TopBarComponent implements OnInit, OnDestroy {
   }
 
   public onLogoClick() {}
+
+  public showMobileMenuClicked() {
+    this.showMobileMenu = true;
+  }
+
+  public hideMobileMenuClicked() {
+    this.showMobileMenu = false;
+  }
 }
