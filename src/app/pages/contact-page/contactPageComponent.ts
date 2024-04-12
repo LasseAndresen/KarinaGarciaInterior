51768830cda52aab5e0de9a8f31c1bc5ba00e705
Lasse <lasse.andresen9@gmail.com>
2024-04-12 22:05:23 +0200

    Contact form submit now calls API endpoint

--- src/app/pages/contact-page/contactPageComponent.ts
+++ src/app/pages/contact-page/contactPageComponent.ts
@@ -1,4 +1,5 @@
 import {Component, HostBinding} from "@angular/core";
+import {environment} from "../../../environment";
 
 @Component({
     selector: 'contact-page',
@@ -19,11 +20,23 @@ export class ContactPageComponent {
     protected onSubmit() {
         console.log('form submitted', this.form);
         this.submitted = true;
-        this.form = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            message: ''
-        };
+
+        fetch(environment.apiUrl + '/submitContactForm', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.form),
+        })
+            .then(() => {
+                this.submitted = true;
+                this.form = {
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    message: ''
+                };
+            })
+            .catch(err => console.error(err));
     }
 }
